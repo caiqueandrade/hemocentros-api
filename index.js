39ac498f1096ae7223c9941f3bcac9cc66c5ced4
Caique Andrade <caiquembandrade@gmail.com>
2017-07-13 20:58:01 -0300

    Atualização para colocar o IP do servidor

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ app.use(bodyParser.json());
 
 app.use(expressMongoDb('mongodb://localhost:27017/hemocentros-api'));
 
-app.listen(3000, function(){
-    console.log('Acesse o servidor http://localhost:3000');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+app.listen(3000, function(){
+    console.log('Acesse o servidor http://174.138.68.160:3000');
+});
+
 
 var hemocentrosController = require('./controllers/hemocentros.js');
 
